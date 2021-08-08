5de2e6b3907b930fb9a5a5e40d26748958fd57df
dtinth on MBP M1 <dtinth@spacet.me>
2021-08-08 23:56:46 +0700

    Fix lint error

--- src/helpers/repoUrl.js
+++ src/helpers/repoUrl.js
@@ -2,6 +2,6 @@
 
 module.exports = (page) => {
   const x = page && (page.editUrl || page.fileUrl)
-  const m = x && x.match(/^https?:\/\/[^\/]+\/[^\/]+\/[^\/]+/i)
-  return m && m[0] || 'https://github.com/dtinth/docs'
+  const m = x && x.match(/^https?:\/\/[^/]+\/[^/]+\/[^/]+/i)
+  return m ? m[0] : 'https://github.com/dtinth/docs'
 }
