73b4469bdf4accbc66692e97c52bde94868ae743
dtinth on MBP M1 <dtinth@spacet.me>
2021-08-08 23:55:42 +0700

    Link GitHub to parent repo

---
+++ src/helpers/repoUrl.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = (page) => {
+  const x = page && (page.editUrl || page.fileUrl)
+  const m = x && x.match(/^https?:\/\/[^\/]+\/[^\/]+\/[^\/]+/i)
+  return m && m[0] || 'https://github.com/dtinth/docs'
+}
